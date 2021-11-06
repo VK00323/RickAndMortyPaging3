aeb463aa3dba27a6a6b1aa5a442dc5c187a2c5b0
Vadim Kuznetsov <90134447+VK00323@users.noreply.github.com>
2021-11-06 15:12:42 +0500

    Создана база данных

---
+++ app/src/main/java/com/example/rickandmortypaging3/database/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.rickandmortypaging3.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.rickandmortypaging3.network.model.PojoInfo
+
+@Database(entities = [PojoInfo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        private const val DB_NAME = "main.db"
+        private var db: AppDatabase? = null
+        private val LOCK = Any()
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(LOCK) {
+                val instance = Room.databaseBuilder(
+                    context, AppDatabase::class.java, DB_NAME
+                )
+                    .build()
+                db = instance
+                return instance
+            }
+        }
+    }
+    abstract fun characterDao(): CharacterDao
+}
